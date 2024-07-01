766cad89565a5ad97d95db13e6d7b5be12d27617
Onur Er <onurer@gmail.com>
2024-07-01 13:07:43 -0700

    Deleted our custom Range related parameter validation aspects because they are provided by Metalama.Patterns.Contracts library now.

---
+++ Atesh.Metalama/InequalityStrictnessFabric.cs
@@ -0,0 +1,9 @@
+﻿using Metalama.Framework.Fabrics;
+using Metalama.Patterns.Contracts;
+
+namespace Atesh.Metalama;
+
+class InequalityStrictnessFabric : TransitiveProjectFabric
+{
+    public override void AmendProject(IProjectAmender Amender) => Amender.ConfigureContracts(new() { DefaultInequalityStrictness = InequalityStrictness.Strict });
+}
