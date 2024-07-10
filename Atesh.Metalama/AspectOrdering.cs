8adf1d7d40308472e927934f7e8d1b3d9c1142ff
Onur Er <onurer@gmail.com>
2024-07-10 12:34:17 -0700

    Fixed the aspect ordering.

--- Atesh.Metalama/AspectOrdering.cs
+++ Atesh.Metalama/AspectOrdering.cs
@@ -1,10 +1,5 @@
 ﻿using Metalama.Framework.Aspects;
 using Atesh.Metalama;
-/*
-[assembly: AspectOrder(typeof(NotNullAttribute),
-    typeof(GreaterThanAttribute), 
-    typeof(GreaterThanOrEqualAttribute), 
-    typeof(LessThanAttribute), 
-    typeof(LessThanOrEqualAttribute), 
-    typeof(RangeAttribute), 
-    typeof(ValidateOperationAttribute))]*/
+using Metalama.Patterns.Contracts;
+
+[assembly: AspectOrder(AspectOrderDirection.RunTime, typeof(ContractBaseAttribute), typeof(ValidateOperationAttribute))]
